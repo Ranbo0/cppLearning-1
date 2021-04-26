3ff4c6f2a81bc563aab1d66178b4ee55e31f5d9c
Ranbo0 <605139189@qq.com>
2021-04-26 15:29:33 +0800

    Redefinition of a macro definition

---
+++ define.cpp
@@ -0,0 +1,26 @@
+/********************************************************************************
+ *
+ * @author: Cao Xingyu
+ * @date: 2020-04-26
+ * @description: Redefinition of a macro definition
+ * @version: 1.0
+ * @conditions: -
+ *
+ ********************************************************************************/
+
+#include <bits/stdc++.h>
+
+#define A 1000
+#define A 100
+#define AA 10
+
+using namespace std;
+
+int main()
+{
+    cout << "A = " << A << endl;    // A = 100
+    // macro definition can be redefined, and the final definition is the last one.
+
+    system("pause");
+    return 0;
+}
